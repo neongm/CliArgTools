399f98cea8a46c86a4a36da62e07c35fe6713c1e
neongm <chura2013c@gmail.com>
2022-10-04 09:59:01 +0500

    added setup and configs

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CliArgTools',
+    version='0.1',
+    license='MIT',
+    author="Vologin Alexander",
+    author_email='chura2013c@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://https://github.com/neongm/CliArgTools',
+    keywords='cli arguments',
+    install_requires=[
+      ],
+)
